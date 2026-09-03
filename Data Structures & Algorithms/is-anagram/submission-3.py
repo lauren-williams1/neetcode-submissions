7d55f33783ce9c4011b833570c23c4a3d82955bc
Lauren <41021434+lauren-williams1@users.noreply.github.com>
2026-09-03 09:09:53 -0500

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        count = defaultdict(int)
+
+        for char in s:
+            count[char] += 1
+        
+        for char in t:
+            count[char] -= 1
+        
+        for val in count.values():
+            if val != 0:
+                return False
+        return True
+
+        
+
+                
+       
